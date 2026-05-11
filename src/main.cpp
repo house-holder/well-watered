a4b4502cad0d6d8885f1c4f497b7166b292bf7a9
kh <kylemhouseholder@gmail.com>
2026-05-11 07:28:26 -0500

    build: load json schedule

--- src/main.cpp
+++ src/main.cpp
@@ -45,28 +45,83 @@ void updateLED(StatLED &LED) {
 StatLED Ok =	 { 13, 100, 4900, OFF, 0 };
 StatLED Warn =	 { 14, 250, 1750, OFF, 0 };
 
+struct Schedule {
+	bool days[7];
+	int startHour;
+	int startMin;
+	int stopHour;
+	int stopMin;
+};
+
+const Schedule DEFAULT_SCHEDULE = {
+    .days  = { false, false, false, false, false, false, false },
+    .startHour = 6, .startMin = 0, .stopHour  = 7, .stopMin  = 15
+};
+
+void saveSchedule() {
+	JsonDocument doc;
+	JsonArray arr = doc["zones"].to<JsonArray>();
+	for (int i = 0; i < 3; i++) {
+		JsonObject obj = arr.add<JsonObject>();
+		obj["id"] = i;
+		JsonArray days = obj["days"].to<JsonArray>();
+		for (int d = 0; d < 7; d++) {
+			days.add(zones[i].schedule.days[d]);
+		}
+		obj["startHour"] = zones[i].schedule.startHour;
+		obj["startMin"] = zones[i].schedule.startMin;
+		obj["stopHour"] = zones[i].schedule.stopHour;
+		obj["stopMin"] = zones[i].schedule.stopMin;
+	}
+	File f = LittleFS.open("/save.json", "w");
+	serializeJson(doc, f);
+	f.close();
+}
+
+void loadSchedule() {
+    if (!LittleFS.exists("/save.json")) {
+        Serial.println("No save.json found, using defaults");
+        return;
+    }
+    File f = LittleFS.open("/save.json", "r");
+    JsonDocument doc;
+    DeserializationError err = deserializeJson(doc, f);
+    f.close();
+
+    if (err) {
+        Serial.println("Failed to parse save.json");
+        return;
+    }
+
+	for (i = 0; i < 3; i++) { // load each zone
+		zones[i].schedule.startHour = doc["zones"][i]["startHour"];
+		zones[i].schedule.startMin = doc["zones"][i]["startMin"];
+		zones[i].schedule.startHour = doc["zones"][i]["startHour"];
+		zones[i].schedule.startMin = doc["zones"][i]["startMin"];
+		for (d = 0; d < 7; d++ ) { // load each day
+
+		}
+	}
+
+}
+
 struct Zone {
 	int pin;
 	bool running;
 	const char* name;
 	time_t startTime;
+	Schedule schedule;
 	StatLED indication;
 };
 
 Zone zones[3] = {
-	{ 21, false, "Garden faucet", 0, { 16, 200, 800, OFF, 0 } },
-	{ 22, false, "House faucet",  0, { 17, 200, 800, OFF, 0 } },
-	{ 23, false, "Shed faucet",   0, { 18, 200, 800, OFF, 0 } },
+	{ .pin=21, .name="Garden faucet", .schedule=DEFAULT_SCHEDULE,
+		.indication={ .pin=16, .onTime=200, .cycleTime=800 }, },
+	{ .pin=22, .name="House faucet",  .schedule=DEFAULT_SCHEDULE,
+		.indication={ .pin=17, .onTime=200, .cycleTime=800 }, },
+	{ .pin=23, .name="Shed faucet",   .schedule=DEFAULT_SCHEDULE,
+		.indication={ .pin=18, .onTime=200, .cycleTime=800 }, },
 };
-
-// Zone zones[3] = {
-//     { .pin=33, .running=false, .name="Garden faucet", .startTime=0,
-//       .indication={ .pin=18, .onTime=200, .cycleTime=800, .mode=OFF, .start=0 } },
-//     { .pin=34, .running=false, .name="House faucet",  .startTime=0,
-//       .indication={ .pin=17, .onTime=200, .cycleTime=800, .mode=OFF, .start=0 } },
-//     { .pin=35, .running=false, .name="Shed faucet",   .startTime=0,
-//       .indication={ .pin=16, .onTime=200, .cycleTime=800, .mode=OFF, .start=0 } },
-// };
 
 String getStateJSON() {
     JsonDocument doc;
